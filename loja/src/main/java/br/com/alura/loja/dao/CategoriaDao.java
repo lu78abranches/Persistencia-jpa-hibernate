208ea957ee1a15477aafcfbff01833ccc008a7bf
lu78abranches <luisabranches.surf@hotmail.com>
2023-07-07 21:42:31 -0300

    first commit

---
+++ loja/src/main/java/br/com/alura/loja/dao/CategoriaDao.java
@@ -0,0 +1,33 @@
+package br.com.alura.loja.dao;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
+
+public class CategoriaDao {
+	
+	private EntityManager em;
+
+	public CategoriaDao(EntityManager em) {
+		this.em = em;
+	}
+	
+	public void cadastrar(Categoria categoria) {
+		this.em.persist(categoria);
+	}
+	
+	public void atualizar(Categoria categoria) {
+	    this.em.merge(categoria);
+	}
+	
+	public void remover(Categoria categoria) {
+		categoria = em.merge(categoria);
+		this.em.remove(categoria);
+	}
+	
+	
+	
+	
+
+}
